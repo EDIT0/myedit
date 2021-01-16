462880b9ecbef64324c6239044fe4a791c48b84d
edit0 <akdmadl34@naver.com>
2021-01-16 18:28:36 +0900

    세탁이

---
+++ user/app/src/main/java/com/example/user/ImageActivity.java
@@ -0,0 +1,145 @@
+package com.example.user;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ImageActivity extends AppCompatActivity {
+    private long backBtnTime = 0;
+    String user_name1,user_address1,user_id1,user_address_detail1;
+    double user_lat1,user_long1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image);
+
+        Intent intent = getIntent();
+        user_name1 = intent.getStringExtra("user_name");
+        user_address1 = intent.getStringExtra("user_address");
+        user_lat1 = intent.getDoubleExtra("user_lat",0.0);
+        user_long1 = intent.getDoubleExtra("user_long",0.0);
+        user_id1 = intent.getStringExtra("user_id");
+        user_address_detail1 = intent.getStringExtra("user_address_detail");
+
+        final String re_set_title = intent.getStringExtra("title");
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle(re_set_title);
+        actionBar.setBackgroundDrawable(new ColorDrawable(0xFF4472C4));
+        actionBar.setDisplayHomeAsUpEnabled(true);
+
+        ViewPager pager = findViewById(R.id.pager);
+        //캐싱을 해놓을 프래그먼트 개수
+        pager.setOffscreenPageLimit(3);
+
+        //getSupportFragmentManager로 프래그먼트 참조가능
+        FragmentStatePagerAdapter adapter = new MoviePagerAdapter(getSupportFragmentManager());
+
+        /*adapter.addItem(fragment1);*/
+
+        user_frag_first fragment2 = new user_frag_first();
+        ((MoviePagerAdapter) adapter).addItem(fragment2);
+
+        user_storereview fragment3 = new user_storereview();
+        ((MoviePagerAdapter) adapter).addItem(fragment3);
+
+        pager.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:{
+                Intent intent = new Intent(getApplicationContext(), user_main1.class);
+                intent.putExtra("user_name",user_name1);
+                intent.putExtra("user_address",user_address1);
+                intent.putExtra("user_lat",user_lat1);
+                intent.putExtra("user_long",user_long1);
+                intent.putExtra("user_id",user_id1);
+                intent.putExtra("user_address_detail",user_address_detail1);
+                setResult(RESULT_OK,intent);
+                finish();
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    //어댑터 안에서 각각의 아이템을 데이터로서 관리한다
+
+    class MoviePagerAdapter extends FragmentStatePagerAdapter {
+        ArrayList<Fragment> items = new ArrayList<Fragment>();
+        public MoviePagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        public void addItem(Fragment item){
+            items.add(item);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return items.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return items.size();
+        }
+    }
+
+    /*@Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+        long curTime = System.currentTimeMillis();
+        long gapTime = curTime - backBtnTime;
+
+        if(0 <= gapTime && 2000 >= gapTime) {
+            moveTaskToBack(true);
+            finish();
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+        else {
+            backBtnTime = curTime;
+            Toast.makeText(this, "한번 더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show();
+        }
+    }*/
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //resultCode가 RESULT_OK면 if문을 실행한다.
+        if (resultCode == RESULT_OK) {
+            
+        }
+    }
+}
